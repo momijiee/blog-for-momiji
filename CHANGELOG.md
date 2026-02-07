6b5550029d67a1b18a0d6d5383a81c06a2aad327
Momiji <momijih1019@gmail.com>
2026-02-07 11:07:22 +0800

    card visual effect

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,39 @@
 # Changelog
 
 
+## [v0.3.0] - 2026-02
+### Added
+- 
+
+### Fixed
+- 
+
+### Changed
+- 
+
+---
+## [v0.2.2] - 2026-02-07
+### Added
+- N/A
+
+### Fixed
+- Improve card visual effect in dark mode
+
+### Changed
+- N/A
+
+---
+## [v0.2.1] - 2026-02-07
+### Added
+- N/A
+
+### Fixed
+- Theme bug of the first click
+
+### Changed
+- N/A
+
+---
 ## [v0.2.0] - 2026-02-06
 ### Added
 - Dark mode support for the entire blog
--- components/ui/animated-post-card.tsx
+++ components/ui/animated-post-card.tsx
@@ -10,37 +10,39 @@ interface AnimatedPostCardProps{
     children: Post
 }
 
+function getPreviewText(content: string) {
+  return content
+    .replace(/^#{1,6}\s+/gm, "") // headings
+    .replace(/!\[[^\]]*\]\([^)]+\)/g, "") // images
+    .replace(/\[[^\]]*\]\([^)]+\)/g, (m) => m.replace(/\[|\]|\([^)]+\)/g, "")) // links -> text
+    .replace(/`{1,3}[^`]*`{1,3}/g, "") // inline code
+    .replace(/[*_~>-]+/g, "") // common markdown tokens
+    .replace(/\n+/g, " ")
+    .trim();
+}
+
 const cardVariants = {
-    hover: {
-        y: -6,
-        boxShadow: "0 20px 25px -5px rgb(0 0 0 / 0.1), 0 10px 10px -5px rgb(0 0 0 / 0.04)"
-    },
+    hover: { y: -6 },
     tap: { scale: 0.98 }
 }
 
-function getPreviewText(content: string) {
-    return content
-      .replace(/^#{1,6}\s+/gm, "") // headings
-      .replace(/!\[[^\]]*\]\([^)]+\)/g, "") // images
-      .replace(/\[[^\]]*\]\([^)]+\)/g, (m) => m.replace(/\[|\]|\([^)]+\)/g, "")) // links -> text
-      .replace(/`{1,3}[^`]*`{1,3}/g, "") // inline code
-      .replace(/[*_~>-]+/g, "") // common markdown tokens
-      .replace(/\n+/g, " ")
-      .trim();
-}
 
 export function AnimatedPostCard({ children }:AnimatedPostCardProps) {
     return(
         <Link href={`/blog/${children.slug}`}>
             <motion.div
-                  className="overflow-hidden rounded-xl inline-block"
                   variants={cardVariants}
                   whileHover="hover"
                   whileTap="tap"
                   transition={{ type: "spring", stiffness: 300, damping: 30}}
             >
-            <Card className="overflow-hidden cursor-pointer transition-colors">
-                
+              <Card className="
+                overflow-hidden rounded-xl inline-block
+                transition-shadow shadow-md       
+                hover:shadow-xl 
+                dark:ring-1 ring-white/10
+                dark:hover:ring-white/30
+              ">
                 {children.image && (
                   <div className="relative aspect-[2/1] w-full">
                     <Image
@@ -62,7 +64,7 @@ export function AnimatedPostCard({ children }:AnimatedPostCardProps) {
                     {children.createdAt}
                   </p>
                 </CardContent>
-            </Card>
+              </Card>
             </motion.div>
         </Link>
     )
